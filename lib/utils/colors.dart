78b2ee9bc3c0cde773a34904fcc47f90ce57e8c7
smuts101 <69514780+smuts101@users.noreply.github.com>
2022-02-27 16:15:52 +0200

    Slide show and half the home page almost done

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors{
+  static final Color mainColor = const Color(0xFF89dad0);
+  static final Color iconColor1  = const Color(0xFFffd28d);
+  static final Color iconColor2  = const Color(0xFFfcab88);
+
+
+}
