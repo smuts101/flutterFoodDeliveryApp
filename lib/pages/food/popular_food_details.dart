44922c242e330441622a19d346841fad35402546
smuts101 <69514780+smuts101@users.noreply.github.com>
2022-03-12 07:56:10 +0200

    done with pages

--- lib/pages/food/popular_food_details.dart
+++ lib/pages/food/popular_food_details.dart
@@ -139,11 +139,12 @@ class PopularFoodDetail extends StatelessWidget {
               ),
             ),
             Container(
+
               padding: EdgeInsets.only(top: Dimensions.height20,bottom: Dimensions.height20,left: Dimensions.width20,right: Dimensions.width20),
               child: BigText(text: "\$10 | Add to cart",color: Colors.white,),
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(Dimensions.radius20),
-                color: AppColors.mainColor
+                  borderRadius: BorderRadius.circular(Dimensions.radius20),
+                  color: AppColors.mainColor
               ),
             )
             
